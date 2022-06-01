5255df1178ea30f86f472f89389abb80a6480cad
Danil Lysak <lysakdanil@gmail.com>
2022-06-01 18:53:35 +0300

    Add files via upload

---
+++ src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="home">
+        <h1 class="title">Welcome to APP!</h1>
+        <img src="@/assets/background.svg" alt="img">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
